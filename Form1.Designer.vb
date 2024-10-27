f2347dda912d79d2e547a78bb823ff213191547c
NaughtDZ <1603705420@qq.com>
2024-10-27 16:32:55 +0800

    添加项目文件。

---
+++ Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        TextBox1 = New TextBox()
+        TrackBar1 = New TrackBar()
+        Button3 = New Button()
+        CType(TrackBar1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(71, 576)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(119, 44)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(312, 576)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(119, 42)
+        Button2.TabIndex = 1
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(12, 12)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.ScrollBars = ScrollBars.Both
+        TextBox1.Size = New Size(1117, 427)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TrackBar1
+        ' 
+        TrackBar1.Location = New Point(967, 573)
+        TrackBar1.Name = "TrackBar1"
+        TrackBar1.Size = New Size(104, 45)
+        TrackBar1.TabIndex = 3
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(452, 579)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(139, 37)
+        Button3.TabIndex = 4
+        Button3.Text = "Button3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 17F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1141, 630)
+        Controls.Add(Button3)
+        Controls.Add(TrackBar1)
+        Controls.Add(TextBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Book Reader For GPT-SoVITSv2"
+        CType(TrackBar1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Button3 As Button
+
+End Class
