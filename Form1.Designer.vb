50da69ec513ec27cd569cd3e887ac1fcaa741adc
NaughtDZ <1603705420@qq.com>
2024-10-27 20:05:19 +0800

    v0.1 test

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,4 +1,7 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+﻿Imports System.IO
+Imports LibVLCSharp.Shared
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
@@ -22,30 +25,35 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Button1 = New Button()
         Button2 = New Button()
         TextBox1 = New TextBox()
-        TrackBar1 = New TrackBar()
         Button3 = New Button()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TrackBar1 = New TrackBar()
+        VlcControl1 = New Vlc.DotNet.Forms.VlcControl()
         CType(TrackBar1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(VlcControl1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(71, 576)
+        Button1.Location = New Point(12, 581)
         Button1.Name = "Button1"
-        Button1.Size = New Size(119, 44)
+        Button1.Size = New Size(100, 40)
         Button1.TabIndex = 0
-        Button1.Text = "Button1"
+        Button1.Text = "提交播放"
         Button1.UseVisualStyleBackColor = True
         ' 
         ' Button2
         ' 
-        Button2.Location = New Point(312, 576)
+        Button2.Location = New Point(118, 581)
         Button2.Name = "Button2"
-        Button2.Size = New Size(119, 42)
+        Button2.Size = New Size(100, 40)
         Button2.TabIndex = 1
-        Button2.Text = "Button2"
+        Button2.Text = "停止所有播放"
         Button2.UseVisualStyleBackColor = True
         ' 
         ' TextBox1
@@ -56,36 +64,75 @@ Partial Class Form1
         TextBox1.ScrollBars = ScrollBars.Both
         TextBox1.Size = New Size(1117, 427)
         TextBox1.TabIndex = 2
-        ' 
-        ' TrackBar1
-        ' 
-        TrackBar1.Location = New Point(967, 573)
-        TrackBar1.Name = "TrackBar1"
-        TrackBar1.Size = New Size(104, 45)
-        TrackBar1.TabIndex = 3
+        TextBox1.Text = "先帝创业未半而中道崩殂，今天下三分，益州疲弊，此诚危急存亡之秋也。"
         ' 
         ' Button3
         ' 
-        Button3.Location = New Point(452, 579)
+        Button3.Location = New Point(224, 581)
         Button3.Name = "Button3"
-        Button3.Size = New Size(139, 37)
+        Button3.Size = New Size(100, 40)
         Button3.TabIndex = 4
-        Button3.Text = "Button3"
+        Button3.Text = "暂停/恢复"
         Button3.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(12, 454)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.ScrollBars = ScrollBars.Both
+        TextBox2.Size = New Size(1117, 42)
+        TextBox2.TabIndex = 5
+        TextBox2.Text = "http://127.0.0.1:9880/tts?text="
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(12, 502)
+        TextBox3.Multiline = True
+        TextBox3.Name = "TextBox3"
+        TextBox3.ScrollBars = ScrollBars.Both
+        TextBox3.Size = New Size(1117, 41)
+        TextBox3.TabIndex = 6
+        TextBox3.Text = resources.GetString("TextBox3.Text")
+        ' 
+        ' TrackBar1
+        ' 
+        TrackBar1.Location = New Point(330, 581)
+        TrackBar1.Maximum = 100
+        TrackBar1.Name = "TrackBar1"
+        TrackBar1.Size = New Size(799, 45)
+        TrackBar1.TabIndex = 8
+        TrackBar1.Value = 50
+        ' 
+        ' VlcControl1
+        ' 
+        VlcControl1.BackColor = Color.Black
+        VlcControl1.Location = New Point(856, 564)
+        VlcControl1.Name = "VlcControl1"
+        VlcControl1.Size = New Size(75, 23)
+        VlcControl1.Spu = -1
+        VlcControl1.TabIndex = 9
+        VlcControl1.Text = "VlcControl1"
+        VlcControl1.VlcLibDirectory = New DirectoryInfo(".\libvlc\win-x64")
+        VlcControl1.VlcMediaplayerOptions = Nothing
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 17F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(1141, 630)
-        Controls.Add(Button3)
+        Controls.Add(VlcControl1)
         Controls.Add(TrackBar1)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(Button3)
         Controls.Add(TextBox1)
         Controls.Add(Button2)
         Controls.Add(Button1)
         Name = "Form1"
         Text = "Book Reader For GPT-SoVITSv2"
         CType(TrackBar1, ComponentModel.ISupportInitialize).EndInit()
+        CType(VlcControl1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -93,7 +140,10 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TrackBar1 As TrackBar
     Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents VlcControl1 As Vlc.DotNet.Forms.VlcControl
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,124 +1,37 @@
 ﻿Imports System.IO
 Imports System.Net.Http
-Imports System.Threading
-Imports System.Threading.Tasks
-Imports System.Windows.Forms.VisualStyles.VisualStyleElement
-Imports NAudio.Wave
+Imports LibVLCSharp.Shared
+Imports Vlc.DotNet.Forms
+Imports System.Web
 Public Class Form1
-    Private cancellationTokenSource As CancellationTokenSource
-    Private audioOutput As WaveOutEvent
-    Private audioStream As MemoryStream
-    Private audioReader As WaveStream
-    Private isPaused As Boolean = False ' 用于追踪音频是否暂停
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 
     Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        ' 停止之前的操作
-        StopAudio()
+        Dim url As String = TextBox2.Text & HttpUtility.UrlEncode(TextBox1.Text) & TextBox3.Text
+        MsgBox(url)
+        VlcControl1.Stop()
 
-        ' 初始化新的取消令牌
-        cancellationTokenSource = New CancellationTokenSource()
-        Dim token = cancellationTokenSource.Token
+        VlcControl1.SetMedia(url, String.Empty)
 
-        ' 获取用户输入的长文本
-        Dim longText As String = TextBox1.Text
-        Dim paragraphs() As String = longText.Split(New String() {Environment.NewLine & Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries)
-
-        Try
-            For i As Integer = 0 To paragraphs.Length - 1
-                If token.IsCancellationRequested Then
-                    Exit For
-                End If
-
-                ' 发送网络请求并获取音频
-                Dim audioData As Byte() = Await SendRequest(paragraphs(i), token)
-                If audioData IsNot Nothing Then
-                    ' 播放音频
-                    PlayAudio(audioData)
-
-                    ' 等待音频播放完成
-                    Await Task.Run(Sub()
-                                       While audioOutput.PlaybackState = PlaybackState.Playing OrElse isPaused
-                                           Thread.Sleep(100)
-                                       End While
-                                   End Sub, token)
-                End If
-            Next
-        Catch ex As OperationCanceledException
-            ' 操作被取消
-        Finally
-            StopAudio()
-        End Try
+        VlcControl1.Play()
     End Sub
 
-    Private Async Function SendRequest(text As String, token As CancellationToken) As Task(Of Byte())
-        ' 网络请求发送函数
-        Dim client As New HttpClient()
-        Try
-            Dim response As HttpResponseMessage = Await client.GetAsync("http://your-api-url?text=" & text, token)
-            response.EnsureSuccessStatusCode()
-            Return Await response.Content.ReadAsByteArrayAsync()
-        Catch ex As Exception
-            MessageBox.Show("请求失败: " & ex.Message)
-            Return Nothing
-        End Try
-    End Function
-
-    Private Sub PlayAudio(audioData As Byte())
-        StopAudio()
-
-        ' 初始化内存流和NAudio音频流
-        audioStream = New MemoryStream(audioData)
-        audioReader = New WaveFileReader(audioStream)
-        audioOutput = New WaveOutEvent()
-        audioOutput.Init(audioReader)
-        audioOutput.Play()
-    End Sub
-
-    Private Sub StopAudio()
-        If audioOutput IsNot Nothing Then
-            audioOutput.Stop()
-            audioOutput.Dispose()
-            audioOutput = Nothing
-        End If
-        If audioReader IsNot Nothing Then
-            audioReader.Dispose()
-            audioReader = Nothing
-        End If
-        If audioStream IsNot Nothing Then
-            audioStream.Dispose()
-            audioStream = Nothing
-        End If
-        isPaused = False ' 重置暂停状态
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'VlcControl1.VlcLibDirectory = New DirectoryInfo(".\libvlc\win-x64")
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        ' 停止播放按钮事件
-        If cancellationTokenSource IsNot Nothing Then
-            cancellationTokenSource.Cancel()
-        End If
-        StopAudio()
+        VlcControl1.VlcMediaPlayer.Stop()
+    End Sub
+
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        VlcControl1.Audio.Volume = TrackBar1.Value
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        ' 暂停/恢复按钮事件
-        If audioOutput IsNot Nothing Then
-            If audioOutput.PlaybackState = PlaybackState.Playing Then
-                audioOutput.Pause()
-                isPaused = True
-            ElseIf isPaused Then
-                audioOutput.Play()
-                isPaused = False
-            End If
-        End If
-    End Sub
-
-    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs)
-        ' 调节音量，TrackBar1的值在0到100之间
-        If audioOutput IsNot Nothing Then
-            audioOutput.Volume = TrackBar1.Value / 100.0F
+        If VlcControl1.VlcMediaPlayer.IsPlaying = True Then
+            VlcControl1.Pause()
+        Else
+            VlcControl1.VlcMediaPlayer.Play()
         End If
     End Sub
 End Class
